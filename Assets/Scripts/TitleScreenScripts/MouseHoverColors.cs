386a89f1c43d89b008e5990741366fa059834211
quazarxx <mysubs666@gmail.com>
2021-10-22 23:08:55 -0400

    Changed text to TextMeshPro

--- Assets/Scripts/TitleScreenScripts/MouseHoverColors.cs
+++ Assets/Scripts/TitleScreenScripts/MouseHoverColors.cs
@@ -3,18 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
 
 public class MouseHoverColors : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public Text text;
+    public TextMeshProUGUI text;
+
+    void Awake () 
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         text.color = new Color32(151, 41, 54, 255);
+
+        text.fontMaterial.SetFloat(ShaderUtilities.ID_GlowPower, 1.0f);
+        text.UpdateMeshPadding();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         text.color = new Color32(50, 50, 50, 255);
+
+        text.fontMaterial.SetFloat(ShaderUtilities.ID_GlowPower, 0.0f);
+        text.UpdateMeshPadding();
     }
 }
