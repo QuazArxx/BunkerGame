2a79d65a555337c90c74aa5c6bc1fe0b59817927
QuazArxx <33949974+QuazArxx@users.noreply.github.com>
2021-10-24 23:25:35 -0400

    Updates

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerState : MonoBehaviour
+public class PlayerState
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static int BunkerSize;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public static bool isFiend;
 }
